cf4303b1b2ee0a439013e19fb474c524cf95747f
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2022-07-08 13:52:28 +0200

    Upgrade de la syntaxe en React 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Main from "./main/Main";
 import 'bootswatch/dist/sandstone/bootstrap.min.css';
-ReactDOM.render(
-  <Main/>,
-  document.getElementById("root")
-);
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<React.StrictMode>
+                <Main/>,
+            </React.StrictMode>);
