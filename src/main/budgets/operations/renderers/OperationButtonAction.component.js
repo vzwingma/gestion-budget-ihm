64ff416c0e7d6003f77044c3e95ffc664947f945
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2022-10-09 21:44:28 +0200

    migration bootstrap vers mui

--- src/main/budgets/operations/renderers/OperationButtonAction.component.js
+++ src/main/budgets/operations/renderers/OperationButtonAction.component.js
@@ -1,13 +1,14 @@
-import {Tooltip} from '@mui/material';
-import {Button} from 'react-bootstrap';
+import {Tooltip, Button} from '@mui/material';
+
 /*
  * Composant Bouton pour actions sur la liste des opérations
  */
 const OperationButtonAction = ({ action, iconAction, label}) => {
 
     return (
+
         <Tooltip title={label}>
-            <Button className="btn-light" variant="light">
+            <Button className="btn-actions-operation">
                 <img id={action} src={"/img/statuts/" + iconAction} width="20" height="20" className="d-inline-block align-top" alt={label}/>
             </Button>
         </Tooltip>
