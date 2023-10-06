e43739f55c0b615eb417cfdd7f6f44e41d963a82
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2023-10-06 20:16:32 +0200

    fix sauvegarde d'une opération sans changement de valeur

--- src/main/budgets/operations/detail/OperationDetailPage.controller.js
+++ src/main/budgets/operations/detail/OperationDetailPage.controller.js
@@ -60,17 +60,19 @@ export function handleValidateOperationForm() {
         }
 
         // Valeur
-        if (this.state.editOperation.valeur === null || this.state.editOperation.valeur === "") {
-            errors.valeur = "Le champ Valeur est obligatoire";
-            hasErrors = true;
-        } else {
-            let valeur = ("" + this.state.editOperation.valeur).replaceAll(",", ".");
-
-            if (!/(^\d*.\d{2}$)/.test(valeur)) {
-                errors.valeur = "Le format est incorrect : 0000.00 €";
+        if (this.state.editForm.value) {
+            if (this.state.editOperation.valeur === null || this.state.editOperation.valeur === "") {
+                errors.valeur = "Le champ Valeur est obligatoire";
                 hasErrors = true;
             } else {
-                this.props.operation.valeur = (this.state.editOperation.typeOperation === "DEPENSE" ? -1 : 1) * valeur;
+                let valeur = ("" + this.state.editOperation.valeur).replaceAll(",", ".");
+                console.log(valeur)
+                if (!/(^\d*.\d{2}$)/.test(valeur)) {
+                    errors.valeur = "Le format est incorrect : 0000.00 €";
+                    hasErrors = true;
+                } else {
+                    this.props.operation.valeur = (this.state.editOperation.typeOperation === "DEPENSE" ? -1 : 1) * valeur;
+                }
             }
         }
 
