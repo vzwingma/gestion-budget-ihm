5d9b43461014413451b1c58cfa4d040a99432b27
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2023-10-12 15:58:14 +0200

    #135

--- src/main/budgets/budget/Budget.component.js
+++ src/main/budgets/budget/Budget.component.js
@@ -5,11 +5,12 @@ import * as Controller from './Budget.controller'
 import * as Services from './Budget.extservices'
 import Grid2 from "@mui/material/Unstable_Grid2";
 import BudgetTitre from "./BudgetTitre.component";
-import {Box, CircularProgress, Divider, TextField} from "@mui/material";
+import {Box, CircularProgress, Divider, InputBase, Paper} from "@mui/material";
 import OperationDetailPage from "../operations/detail/OperationDetailPage.component";
 import MenuIcon from '@mui/icons-material/Menu';
 import OperationsListe from "../operations/OperationsListe.component";
 import PropTypes from "prop-types";
+import {SearchRounded} from "@mui/icons-material";
 
 /**
  * Page principale des budgets
@@ -83,6 +84,9 @@ export default class Budget extends Component {
             console.log("[TRIGGER] Context operation=" + (nextStates.currentOperation !== null ? nextStates.currentOperation.id : "Nouvelle opération"))
             componentUpdate = true;
         }
+        if (this.state.filterOperations !== nextStates.filterOperations) {
+            componentUpdate = true;
+        }
         return componentUpdate;
     }
 
@@ -95,8 +99,17 @@ export default class Budget extends Component {
             <Grid2 container marginTop={1} sx={{overflow: "hidden"}}>
                 <Grid2 md={0.3}><MenuIcon onClick={this.props.onOpenMenu} className={"editableField"}
                                           fontSize={"large"}/></Grid2>
-                <Grid2 md={2.7} paddingTop={"10px"}>
-                    <TextField id="search-basic" fullWidth size={"small"} onChange={this.handleOperationFilter}/>
+                <Grid2 md={2.7} paddingTop={"6px"}>
+                    <Paper component="form"
+                           sx={{p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%'}}>
+                        <InputBase
+                            sx={{ml: 1, flex: 1, color: "#808080"}}
+                            placeholder="Filtrage des opérations"
+                            inputProps={{'aria-label': 'Filtrage des opérations'}}
+                            onChange={this.handleOperationFilter}
+                        />
+                        <SearchRounded/>
+                    </Paper>
                 </Grid2>
                 <Grid2 md={2}></Grid2>
                 <Grid2 md={6}>
@@ -124,6 +137,7 @@ export default class Budget extends Component {
                     { /** Liste des opérations **/
                         (this.state.currentBudget != null ?
                                 <OperationsListe operationGroupedByDate={this.state.operationsGroupedByDateOperation}
+                                                 filterOperations={this.state.filterOperations}
                                                  listeComptes={this.props.listeComptes}
                                                  onClick={this.handleOperationSelect}/>
                                 :
