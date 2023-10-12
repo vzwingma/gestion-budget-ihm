df4cb0c2c3ae85707ae25791c2c23029b89e00df
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2023-10-12 20:46:51 +0200

    profile

--- src/main/menuTopBar/Profile.component.js
+++ src/main/menuTopBar/Profile.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useAuth} from "react-oidc-context";
-import {CircularProgress, Tooltip, Typography} from "@mui/material";
+import {Avatar, CircularProgress, Tooltip, Typography} from "@mui/material";
 import {putTokenInStorage, removeTokenFromStorage} from '../Services/Auth.service'
 import ProfileInfos from "./ProfileInfos.component";
 
@@ -31,8 +31,11 @@ function Profile() {
 
     if (auth.error) {
         console.log("Erreur lors de l'authentification : " + auth.error.message)
+        removeTokenFromStorage();
     }
 
+
+    // Si l'utilisateur est connecté
     if(auth.isAuthenticated) {
         putTokenInStorage(auth.user?.id_token)
         return (
@@ -42,21 +45,23 @@ function Profile() {
                 <Typography variant={"subtitle1"} component="div" sx={{flexGrow: 10, marginLeft: "20px"}}
                             align={"right"}>
                     <Tooltip title={  auth.user?.profile.name }>
-                        <img onClick={logOut} src={auth.user?.profile.picture} width="62" height="62"
-                             style={{margin: "2px"}} alt="User loggé" referrerPolicy="no-referrer"/>
+                        <Avatar onClick={logOut} src={auth.user?.profile.picture}
+                                style={{margin: "2px", width: "62px", height: "62px"}} alt="User loggé"
+                                referrerPolicy="no-referrer"/>
                     </Tooltip>
                 </Typography>
             </>
         );
-    }
-
+    } else {
         return (
-                <Typography variant={"subtitle1"} component="div" sx={{ flexGrow: 10 }} align={"right"}>
-                    <Tooltip title="Non connecté. Cliquez pour vous identifier">
-                        <img onClick={() => auth.signinRedirect()} src="/img/avatar.png" width="60" height="60" alt="User loggé"/>
-                    </Tooltip>
-                </Typography>
+            <Typography variant={"subtitle1"} component="div" sx={{flexGrow: 10}} align={"right"}>
+                <Tooltip title="Non connecté. Cliquez pour vous identifier">
+                    <Avatar onClick={() => auth.signinRedirect()} src="/img/avatar.png"
+                            style={{margin: "2px", width: "62px", height: "62px"}} alt="User loggé"/>
+                </Tooltip>
+            </Typography>
         );
+    }
 }
 export default Profile;
 
