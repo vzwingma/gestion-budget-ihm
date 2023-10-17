6aaaaed22e0fa72d383180e4985e01b7edc03804
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2023-10-17 21:44:01 +0200

    Mise en forme action prévue

--- src/main/budgets/operations/actions/OperationDetailActions.component.js
+++ src/main/budgets/operations/actions/OperationDetailActions.component.js
@@ -67,7 +67,7 @@ export default class OperationDetailActions extends Component {
                 {this.props.currentOperation.etat !== AppConstants.OPERATION_ETATS_ENUM.PREVUE &&
                     <Tooltip title="Prévoir l'opération">
                         <IconButton
-                            className={"buttonsActionsOperations color_" + AppConstants.OPERATION_ETATS_ENUM.PREVUE}
+                            className={"buttonsActionsOperationsP color_" + AppConstants.OPERATION_ETATS_ENUM.PREVUE}
                             id={AppConstants.OPERATION_ETATS_ENUM.PREVUE}>
                             <center>
                                 <HistoryRounded id={AppConstants.OPERATION_ETATS_ENUM.PREVUE}/>
