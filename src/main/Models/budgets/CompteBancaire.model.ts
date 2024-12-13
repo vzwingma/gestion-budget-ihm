2bfcd48b258bf28e1fc52d613073a987eeb514cc
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2024-12-13 18:55:22 +0100

    filtre sur les comptes actifs

--- src/main/Models/budgets/CompteBancaire.model.ts
+++ src/main/Models/budgets/CompteBancaire.model.ts
@@ -6,23 +6,23 @@ class CompteBancaireModel {
     id: string;
     libelle: string;
     itemIcon: string;
-    isDisabled: boolean;
     ordre : number;
+    actif: boolean;
 
     /**
      * Constructor
      * @param id identifiant
      * @param libelle libellé
-     * @param icon icone
-     * @param isDisabled compte désactivé
+     * @param itemIcon icone du compte
      * @param ordre ordre d'affichage
+     * @param actif compte actif
      */
-    constructor(id: string, libelle: string, itemIcon: string, isDisabled: boolean, ordre: number) {
+    constructor(id: string, libelle: string, itemIcon: string, ordre: number, actif: boolean) {
         this.id = id;
         this.libelle = libelle;
         this.itemIcon = itemIcon;
-        this.isDisabled = isDisabled;
         this.ordre = ordre;
+        this.actif = actif;
     }
 }
 export default CompteBancaireModel;  // export default is used to export a single class, function or primitive from a script file.
