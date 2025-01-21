f2bfaa35ad799d1d68fd81ce2943c7557ab53a14
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2025-01-21 18:03:47 +0100

    #226 : correction saisie de date en cours de saisie de form

--- src/main/Components/budgets/operations/detail/subcomponents/OperationDetailDate.component.tsx
+++ src/main/Components/budgets/operations/detail/subcomponents/OperationDetailDate.component.tsx
@@ -1,19 +1,17 @@
-import React, { useContext } from 'react'
-import { OPERATION_EDITION_FORM } from "../OperationDetailPage.constants"
-import { TextField, Typography } from '@mui/material'
-import { getLabelFromDate } from '../../../../../Utils/Date.utils'
-import { OperationDetailDateProps } from '../../../../Components.props'
-import { BudgetContext } from '../../../../../Models/contextProvider/BudgetContextProvider'
-
+import React, {useContext} from 'react'
+import {OPERATION_EDITION_FORM} from "../OperationDetailPage.constants"
+import {TextField, Typography} from '@mui/material'
+import {getLabelFRFromDate, getLabelISOFromDate} from '../../../../../Utils/Date.utils'
+import {OperationDetailDateProps} from '../../../../Components.props'
+import {BudgetContext} from '../../../../../Models/contextProvider/BudgetContextProvider'
 
 
 /**
  * Composant React pour afficher et éditer les détails d'une opération budgétaire.
  *
  * @component
- * 
+ *
  * @property {OperationModel} operation - Le modèle de l'opération.
- * @property {boolean} budgetActif - Indique si le budget est actif.
  * @property {EditFormProps} formDateInEdition - Les propriétés du formulaire d'édition.
  * @property {string | null} errorValeur - Le message d'erreur pour la valeur, ou null s'il n'y a pas d'erreur.
  * @property {(field: OPERATION_EDITION_FORM_IDS.LIBELLE, value: string) => void} fillOperationForm - Fonction pour remplir le formulaire de l'opération.
@@ -26,7 +24,7 @@ export const OperationDetailDate: React.FC<OperationDetailDateProps> = ({   form
     const { currentBudget, currentOperation } = useContext(BudgetContext)!;
     const operation = currentOperation!;
     const budgetActif = currentBudget!.actif;
-    
+
     /**
  * Remplit le champ "dateOperation" de l'état à partir de la saisie de l'utilisateur
  * @param {Event} e - L'événement de saisie
@@ -38,18 +36,18 @@ export const OperationDetailDate: React.FC<OperationDetailDateProps> = ({   form
         }
         fillOperationForm(OPERATION_EDITION_FORM.DATE_OPERATION, value);
     }
-
     return (
+
         (!formDateInEdition) ?
             <Typography id={OPERATION_EDITION_FORM.DATE_OPERATION} variant={"subtitle1"}
                 className={budgetActif ? "editableField" : ""}
                 sx={{ color: (operation.autresInfos.dateOperation != null ? "#FFFFFF" : "#121212") }}>
-                {operation.autresInfos.dateOperation != null ? getLabelFromDate(operation.autresInfos.dateOperation) : "jj/mm/aaaa"}
+                {operation.autresInfos.dateOperation != null ? getLabelFRFromDate(operation.autresInfos.dateOperation) : "jj/mm/aaaa"}
             </Typography>
             :
             <TextField
                 id={OPERATION_EDITION_FORM.DATE_OPERATION + OPERATION_EDITION_FORM.INPUT}
-                defaultValue={operation.autresInfos.dateOperation}
+                defaultValue={operation.autresInfos.dateOperation != null ? getLabelISOFromDate(operation.autresInfos.dateOperation) : null}
                 variant={"standard"} type={"date"} fullWidth
                 error={errorDateOperation != null}
                 helperText={errorDateOperation}
